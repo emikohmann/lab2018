a467fe4fb75e0eb731a73b3d28de26d6f3b1a35f
ekohmann <emiliano.kohmann@mercadolibre.com>
2018-05-11 20:56:10 -0300

    Errores estadtica

--- ejemplos/errores-estadistica/main.go
+++ ejemplos/errores-estadistica/main.go
@@ -2,13 +2,13 @@ package main
 
 import (
     "fmt"
-    "github.com/emikohmann/ubp-lab3/ejemplos/errores-estadistica/tendencia"
+    "github.com/emikohmann/lab2018/ejemplos/errores-estadistica/tendencia"
 )
 
 func main() {
 
     t := tendencia.Tendencia{
-        Valores : []float32{8, 5, 7, 5, 4, 3, 4, 4, 6, 7, 9},
+        Valores : []int{8, 5, 7, 5, 4, 3, 4, 4, 6, 7, 9},
     }
 
     prom, err := t.Promedio()
@@ -17,11 +17,4 @@ func main() {
         return
     }
     fmt.Println("Promedio: ", prom)
-
-    moda, err := t.Moda()
-    if err != nil {
-        fmt.Println("Error: ", err.Error())
-        return
-    }
-    fmt.Println("Moda: ", moda)
 }
--- ejemplos/errores-estadistica/tendencia/tendencia.go
+++ ejemplos/errores-estadistica/tendencia/tendencia.go
@@ -3,23 +3,17 @@ package tendencia
 import "errors"
 
 type Tendencia struct {
-    Valores []float32
+    Valores []int
 }
 
-func Promedio(vs []float32) (float32, error) {
-    if len(vs) == 0 {
-        // La función promedio debería fallar si
-        // el array está vacío
+func (t *Tendencia) Promedio() (float32, error) {
+    if len(t.Valores) == 0 {
         return -1, errors.New("El Array no puede estar vacio")
     }
 
-    var acum float32 = 0
-    for _, v := range vs {
+    acum := 0
+    for _, v := range t.Valores {
         acum += v
     }
-    return acum / float32(len(vs)), nil
-}
-
-func Moda(el []int) int {
-    return 0
+    return float32(acum) / float32(len(t.Valores)), nil
 }
