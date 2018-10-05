732619eb1893c9c7329425167193c8351a03eca2
ekohmann <emiliano.kohmann@mercadolibre.com>
2018-10-05 16:17:17 -0300

    Ejercicio

--- cuatrimestre-2/5-api-productos/main.go
+++ cuatrimestre-2/5-api-productos/main.go
@@ -3,9 +3,12 @@ package main
 import (
     "github.com/gin-gonic/gin"
     productosController "github.com/emikohmann/lab2018/cuatrimestre-2/5-api-productos/controllers/productos"
+    productosService "github.com/emikohmann/lab2018/cuatrimestre-2/5-api-productos/services/productos"
 )
 
 func main() {
+    productosService.LoadProducts()
+
     router := gin.Default()
 
     router.GET("/productos", productosController.GetProductos)
--- cuatrimestre-2/5-api-productos/services/productos/productos_service.go
+++ cuatrimestre-2/5-api-productos/services/productos/productos_service.go
@@ -3,11 +3,24 @@ package productos
 import (
     productosDomain "github.com/emikohmann/lab2018/cuatrimestre-2/5-api-productos/domain/productos"
     "encoding/json"
+    "io/ioutil"
+    "fmt"
 )
 
 var (
     productsList = make([]productosDomain.Producto, 0)
 )
+
+func LoadProducts() {
+    bytes, err := ioutil.ReadFile("./productos.json")
+    if err != nil {
+        panic(err)
+    }
+    if err = json.Unmarshal(bytes, &productsList); err != nil {
+        panic(err)
+    }
+    fmt.Println(productsList)
+}
 
 func GetProductsList() *[]productosDomain.Producto {
     return &productsList
@@ -20,4 +33,8 @@ func AddProduct(body []byte) {
     json.Unmarshal(body, &p)
 
     productsList = append(productsList, p)
+
+    bytes, _ := json.Marshal(&productsList)
+
+    ioutil.WriteFile("./productos.json", bytes, 0644)
 }
